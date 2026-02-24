a90eff5dae3e977201798e9c3f59933bf6dfd213
theaayushirankawat-del <theaayushirankawat@gmail.com>
2026-02-24 17:01:32 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,47 +1,10 @@
-body {
-  margin: 0;
-  height: 100vh;
-  background: linear-gradient(135deg, #ffd1dc, #ffb6c1);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-family: sans-serif;
+function yesClick() {
+  document.querySelector(".card").innerHTML =
+    "<h1>I don’t like you… I LOVE YOU 💞😭</h1>";
 }
 
-.card {
-  background: white;
-  padding: 30px 40px;
-  border-radius: 25px;
-  text-align: center;
-  box-shadow: 0 15px 40px rgba(0,0,0,0.15);
-}
-
-.card h1 {
-  color: #ff6f91;
-}
-
-.card p {
-  color: #ff8fab;
-}
-
-.btns {
-  margin-top: 20px;
-  position: relative;
-}
-
-button {
-  padding: 12px 28px;
-  border: none;
-  border-radius: 20px;
-  font-size: 16px;
-}
-
-.yes {
-  background: #ff6f91;
-  color: white;
-}
-
-.no {
-  background: #ffc2d1;
-  position: absolute;
+function noHover() {
+  const btn = document.querySelector(".no");
+  btn.style.left = Math.random() * 200 + "px";
+  btn.style.top = Math.random() * 100 + "px";
 }
